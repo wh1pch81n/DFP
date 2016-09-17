2d62c3a507307588d987ff304e20f491b3b662c9
Derrick Ho <derrickho328@gmail.com>
2016-09-17 14:28:41 -0400

    dfp and google

---
+++ DFP/DFP.h
@@ -0,0 +1,19 @@
+//
+//  DFP.h
+//  DFP
+//
+//  Created by Derrick  Ho on 9/17/16.
+//  Copyright © 2016 Derrick  Ho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DFP.
+FOUNDATION_EXPORT double DFPVersionNumber;
+
+//! Project version string for DFP.
+FOUNDATION_EXPORT const unsigned char DFPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DFP/PublicHeader.h>
+
+
---
+++ DFP/DFP.swift
@@ -0,0 +1,18 @@
+//
+//  DFP.swift
+//  DFP
+//
+//  Created by Derrick  Ho on 9/17/16.
+//  Copyright © 2016 Derrick  Ho. All rights reserved.
+//
+
+import Foundation
+import GoogleMobileAds
+
+open class DFP {
+	open class func doit() {
+		print("do it")
+		print(DFPRequest.sdkVersion())
+		
+	}
+}
